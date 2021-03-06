e332f95b77f2d957d230a5e3a74a21fbd421b4d4
Andreww02 <wirahadi.andrew@gmail.com>
2021-03-06 12:04:10 +0700

    change to ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+  interface IData {
+    id: string;
+    name: string;
+    email: string;
+  }
+
+  interface IAppState {
+    showTable: boolean;
+  }
+
+  interface IAppProps {
+    data: IData[]
+  }
+
+  class App extends React.Component<IAppProps, IAppState> {
+    public renderTable() {};
+    public toggleShowTable = () => {
+    
+      const{ showTable } = this.state;
+      this.setState({showTable: !showTable});
+    };
+ 
+ public render() {
+   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {this.renderTable()}
+        <button onClick={this.toggleShowTable}>test</button>
       </header>
+      <button onClick={this.toggleShowTable}>test</button>
     </div>
   );
-}
+};
+  };
+
 
 export default App;
